3a7f107ef7c74ed30a69b742c3af067d763e4c21
Firzenax <36512505+Firzenax@users.noreply.github.com>
2021-03-19 20:10:05 +0100

    Update Game.php

--- src/Game.php
+++ src/Game.php
@@ -2,15 +2,43 @@
 
 class Game
 {
+    protected $Player1;
+    protected $Player2;
+
     /**
-     * @param $input 2 lignes, comprenant les infos de chaque joueur
-     * exemple: "Bod 100 7 5\nAlice 80 9 3"
+     * Game constructor.
+     * @param $Player1
+     * @param $Player2
      */
-    public function __construct($input)
+    public function __construct()
     {
+        $this->Player1 = new Player("Bob",30, 5, 2);
+        $this->Player2 = new Player("Jimmy",28,7,4);
     }
+
 
     public function run()
     {
+        echo $this->Player1->getNAME() + "     "+ $this->Player1->getLIFE() + "   " + $this->Player1->getAD() + "   " + $this->Player1->getDEF();
+        echo $this->Player2->getNAME() + "     "+ $this->Player2->getLIFE() + "   " + $this->Player2->getAD() + "   " + $this->Player2->getDEF();
+        while ($this->Player1->getLIFE() > 0 || $this->Player2->getLIFE() > 0){
+            $this->OneTurn();
+            echo $this->Player1->getNAME() + "     "+ $this->Player1->getLIFE() ;
+            echo $this->Player2->getNAME() + "     "+ $this->Player2->getLIFE() ;
+        }
+        if($this->Player1->getLIFE()> 0)
+        {
+            echo $this->Player1->getNAME() + "has" + $this->Player1->getLIFE() + "left";
+        }
+        else
+        {
+            echo $this->Player2->getNAME() + "has" + $this->Player2->getLIFE() + "left";
+        }
+    }
+
+    public function OneTurn()
+    {
+        $this->Player1->setLIFE($this->Player1->getLIFE() - ($this->Player2->getAD() - $this->Player1->getDEF()));
+        $this->Player2->setLIFE($this->Player2->getLIFE() - ($this->Player1->getAD() - $this->Player2->getDEF()));
     }
 }
